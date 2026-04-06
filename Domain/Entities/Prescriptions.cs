dc51158ea88d7d30de6c9bf7156ba9a50b95df0d
Marco Arce <mc8_2001@hotmail.com>
2026-04-06 12:49:57 -0400

    Complementing Prescription Entity documentation

--- Domain/Entities/Prescriptions.cs
+++ Domain/Entities/Prescriptions.cs
@@ -1,15 +1,33 @@
 ﻿namespace Domain.Entities
 {
+    /// <summary>
+    /// Prescription Entity definition
+    /// </summary>
     public class Prescriptions
     {
+        /// <summary>
+        /// Id entifier value
+        /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// Medication for the patient
+        /// </summary>
         public string Medication { get; set; } = string.Empty;
 
+        /// <summary>
+        /// DOsage for the medication
+        /// </summary>
         public string Dosage { get; set; } = string.Empty;
 
+        /// <summary>
+        /// Medicatrion Refils if applies
+        /// </summary>
         public int Refils { get; set; }
 
+        /// <summary>
+        /// Issue date
+        /// </summary>
         public DateOnly IssueDate { get; set; }
 
         public Doctor Doctor { get; set; } = default!;
