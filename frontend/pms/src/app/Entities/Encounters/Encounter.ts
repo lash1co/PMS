3a8cde0b72ba434b113c11e0b9fa4a86343bc947
Marco Arce <mc8_2001@hotmail.com>
2026-05-07 09:57:11 -0400

    FrontEnd: Creating new interfaces to fit with Encounter feature database definition

--- frontend/pms/src/app/Entities/Encounters/Encounter.ts
+++ frontend/pms/src/app/Entities/Encounters/Encounter.ts
@@ -4,4 +4,8 @@ interface EncounterInterface {
   startTime: string;
   endTime: string;
   encounterReason: string;
+  conditions: conditionInterface[];
+  clinicalObservations: clinicalObservationsInterface[];
+  clinicalNotes: clinicalNotesInterface[];
+  prescriptions: prescriptionInterface[];
 }
---
+++ frontend/pms/src/app/Entities/Encounters/prescriptionDetail.ts
@@ -0,0 +1,8 @@
+interface prescriptionDetailInterface {
+  prescriptionId: number;
+  encounterId: number;
+  medicationCode: string;
+  medicationDisplayName: string;
+  dosage: string;
+  refils: number;
+}
